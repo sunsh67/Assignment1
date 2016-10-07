cb71fe4d70d055ff584d646fee6e224707877277
sunsh67 <sun.sh@husky.neu.edu>
2016-10-06 23:54:59 -0700

    delete the question on background

--- src/Assignment4/Stack.java
+++ src/Assignment4/Stack.java
@@ -1,11 +1,5 @@
 package Assignment4;
-/*Design a java class that encapsulate the data structure Stack (Last in First out). The class has 2 methods:
-(push): method for adding items to stack, the push operation adds items to the top of the list
-(pop): method for retrieving items from the stack. The pop operation removes an item from the top of the list, and returns its value to the caller.
-In the case of overflow the user should be informed with a message
-In the case of underflow, the user should be informed with a message & a value of zero is returned.
 
-Assumptions: The stack will hold up to 10 integer values.*/
 public class Stack {
 
 	private int[] values=new int[10];
